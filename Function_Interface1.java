526c5a21b973b3d676fbb7604e0c396c38a9f762
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-03-12 15:26:13 +0530

    just Intro of Functional Interface

---
+++ Function_Interface1.java
@@ -0,0 +1,15 @@
+@FunctionalInterface  //inbuid annotation which stop to create
+//  new FunctinalInterface
+interface First {
+    public void disp();
+    default void output();
+    static void show();
+
+    
+}
+public class Function_Interface1 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Function_Interface2.java
@@ -0,0 +1,13 @@
+@FunctionalInterface
+interface Employee {
+    public void basic_Info();
+
+    
+}
+public class Function_Interface2 {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
