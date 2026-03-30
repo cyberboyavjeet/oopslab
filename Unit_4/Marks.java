9b42643e5e9ed344c89ce2cf4e5ffeb50b90a619
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-03-30 15:23:25 +0530

    Collection FrameWork

---
+++ Unit_4/Marks.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Marks {
+    public static void main(String arg[]){
+        Collection<String>marks=new ArrayList<>();
+        marks.add("5");
+        marks.add("7");
+        marks.add("9");
+        marks.add("19");
+        marks.add("11");
+        System.out.println(marks);
+    }
+}
