91fd7626dac928437560ad44a1b9452911101c17
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-03-30 15:26:09 +0530

    Example of Collection FrameWork

---
+++ Unit_4/CollectionInter.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class CollectionInter{
+    public static void main(String[] args) {
+        Collection<String>list=new ArrayList<>();
+        list.add("Avjeet");
+        list.add("Kumar");
+        System.out.println(list);
+    }
+}
