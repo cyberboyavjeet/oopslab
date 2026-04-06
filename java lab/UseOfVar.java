ee58b4c058738d45b8d13dedbcf5fb2772648572
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-04-06 15:31:49 +0530

    Var in modern java

---
+++ java lab/UseOfVar.java
@@ -0,0 +1,10 @@
+public class UseOfVar {
+    public static void main(String[] args) {
+        var ar="Avjeet ";
+        var a=5;
+        var c=a +" "+ar;
+        System.out.println(c);
+
+    }
+    
+}
