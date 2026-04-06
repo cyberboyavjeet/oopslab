8e0f4df877c5051fe80a9bcb07f563401b720f30
cyberboyavjeet <avjeetsinghk@gmail.com>
2026-04-06 15:38:39 +0530

    Var Application

---
+++ java lab/VarApplication.java
@@ -0,0 +1,8 @@
+public class VarApplication {
+    public static void main(String[] args) {
+        for(var i=0;i<=20;i++){ //local Varriable
+            System.out.println(i);
+            
+        }
+    }
+}
